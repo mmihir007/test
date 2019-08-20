c69cb7eff687c4398837dd30b1cbf286468bc057
mmihir007 <53815496+mmihir007@users.noreply.github.com>
2019-08-20 13:45:29 +0530

    Add files via upload

---
+++ mihirsssss.py
@@ -0,0 +1,14 @@
+def absolute_value(num):
+#this function returns the absolute value of entered numbers
+    if num>=0:
+        return num
+    
+    else:
+        return-num
+            
+    
+#output:2.7
+print(absolute_value(2.7))
+#output:4
+print(absolute_value(4))
+
